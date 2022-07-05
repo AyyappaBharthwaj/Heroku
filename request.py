73d712d72bdbdfab9a19e824379de5fc00f874c5
AyyappaBharthwaj <104905378+AyyappaBharthwaj@users.noreply.github.com>
2022-07-05 20:51:50 +0530

    Delete request.py

--- request.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-url = 'http://localhost:5000/predict_api'
-r = requests.post(url,json={'Maintenance_cost':6000, 'Marketing_cost':5000, 'Profit_Margin':3500, 'Level_of_Course':3, 'Duration_of_coaching_in_Hours':120})
-
-print(r.json())

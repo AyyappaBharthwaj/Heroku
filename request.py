116da267b084a07f2c98921aed72798d0db988c5
AyyappaBharthwaj <104905378+AyyappaBharthwaj@users.noreply.github.com>
2022-07-13 14:41:29 +0530

    Add files via upload

---
+++ request.py
@@ -0,0 +1,6 @@
+import requests
+
+url = 'http://localhost:5000/predict_api'
+r = requests.post(url,json={'Maintenance_cost':6000, 'Marketing_cost':5000, 'Salary_of_the_trainer':3500,'Duration_of_coaching_in_Hours':120,'Mode_of_Class':7,'Level_of_Course':0,'Location':3})
+
+print(r.json())
